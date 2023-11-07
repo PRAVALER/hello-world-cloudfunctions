c9e15f87dec19f76638b9c61e4e3377854108b6e
Rodrigo Ireno <65027944+rodrigo-prv@users.noreply.github.com>
2023-11-07 20:39:43 -0300

    Update index.js

--- cr/src/hello-world/index.js
+++ cr/src/hello-world/index.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+
 exports.helloWorld = function(req, res) {
-    res.send("Hello, World!");
-  };
+  try {
+    const data = fs.readFileSync('/etc/secrets/function-teste1_secret_teste', 'utf8');
+    console.log(data);
+    res.send("Hello, World! -> " + data);
+  } catch (err) {
+    console.error(err);
+    res.send("Error: " + err);
+  }
+};
